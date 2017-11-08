73e5bffe1e80c9f26f35509bfd1a285bef251915
martileos <martinevarez@gmail.com>
2017-11-08 16:07:08 -0700

    Calculadora  basica terminada, randomuser inicio

---
+++ js/calculadora.js
@@ -0,0 +1,58 @@
+//Variable global
+var operador = "";
+function numeros(num){
+	if(operador == ""){ //operando1
+		var valorInicial=document.calculadora.operando1.value;
+		if(valorInicial == "0"){
+			document.calculadora.operando1.value = "";
+		}
+		//Concatena los valores de num 
+		//con los del operando1
+		document.calculadora.operando1.value=
+		document.calculadora.operando1.value + num;
+	}else{ //Operando2
+		var valorInicial=document.calculadora.operando2.value;
+		if(valorInicial == "0"){
+			document.calculadora.operando2.value = "";
+		}
+		//Concatena los valores de num 
+		//con los del operando2
+		document.calculadora.operando2.value=
+		document.calculadora.operando2.value + num;
+	}
+}	
+function operadores(ope){
+	operador = ope
+}
+function igual(){
+	var valor1=document.calculadora.operando1.value;
+	var valor2=document.calculadora.operando2.value;
+	document.calculadora.resultado.value = eval(valor1+operador+valor2);
+}
+function borrar(){
+	operador = "";
+	document.calculadora.operando1.value = 0;
+	document.calculadora.operando2.value = 0;
+	document.calculadora.resultado.value = 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
