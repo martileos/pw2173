567369a61092242b03b6a55c0e431b8f70154515
martileos <martinevarez@gmail.com>
2017-11-09 16:00:17 -0700

    Ejemplo randomuser con boton y muestra foto

--- js/eventosjquery.js
+++ js/eventosjquery.js
@@ -1,14 +1,21 @@
 var inicia = function(){
-	// JSON = JavaScript Object Notation
-	$.ajax({
-	  url: 'https://randomuser.me/api/',
-	  dataType: 'json',
-	  success: function(data) {
-	  	alert(data.results[0].name.first+" "+data.results[0].name.last);
-	    // console.log(data);
-	  }
-	});
+	var nuevo = function(){
+		// JSON = JavaScript Object Notation
+		$.ajax({
+		  url: 'https://randomuser.me/api/?results=20',
+		  dataType: 'json',
+		  success: function(data) {
+		  	//alert(data.results[0].name.first+" "+data.results[0].name.last);
+		    // console.log(data);
+		    $("#nombre").html("<marquee>Nombre: "+data.results[0].name.first+" "+
+		    	              data.results[0].name.last+"</marquee>");
+		    $("#foto").attr("src",data.results[0].picture.large);
+		  }
+		});
+	}
+	
 	// alert("Lista la página");
+	$("#btnNuevo").on("click",nuevo);
 }
 
 //Iniciamos JQuery
