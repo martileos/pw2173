748d5d5aa1b72cfd95ea26a31a4106fdb98d7b00
martileos <martinevarez@gmail.com>
2017-11-17 16:28:56 -0700

    Listado de usuarios con electron con foto

---
+++ listado/index.js
@@ -0,0 +1,32 @@
+
+
+const {BrowserWindow}=require('electron').remote
+const app=require('electron').app;
+const path=require('path');
+const url=require('url');
+const $ = require('jquery');
+function inicia()
+{
+	$.ajax({
+	  url: 'https://randomuser.me/api/?results=20',
+	  dataType: 'json',
+	  success: function(data) {
+	  	var resultado = "";
+	  	var nombre = "";
+	  	for(var i=0;i<20;i++)
+	  	{
+	  		nombre = data.results[i].name.first+" "+data.results[i].name.last;
+	  		foto = data.results[i].picture.medium;
+	  		resultado="<li><img src="+foto+">"+nombre+"</li>";
+	  		$("#lstUsuarios").append(resultado);
+	  	}	  	
+	  }
+	});
+}
+inicia();
+
+
+
+
+
+
---
+++ listado/main.js
@@ -0,0 +1,29 @@
+
+// const app=require('electron').app;
+// const BrowserWindow=require('electron').BrowserWindow;
+const {app,BrowserWindow} = require('electron');
+const path = require('path'); //Muestra la ruta del archivo
+const url = require('url'); //Carga una página
+// ECMASCRIPT = 6
+let PantallaPrincipal;
+
+function muestraPantallaPrincipal(){
+	PantallaPrincipal = new BrowserWindow({width:320,height:425});
+	PantallaPrincipal.loadURL(url.format({
+		pathname: path.join(__dirname,'index.html'),
+		protocol: 'file',
+		slashes: true
+	}))
+	PantallaPrincipal.webContents.openDevTools();
+	PantallaPrincipal.show();
+}
+
+app.on('ready',muestraPantallaPrincipal)
+
+
+
+
+
+
+
+
