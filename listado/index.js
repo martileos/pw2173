395136708891d500a756876fa60f69676dd58790
martileos <martinevarez@gmail.com>
2017-11-21 15:55:38 -0700

    Listado con boton y despliegue de datos

--- listado/index.js
+++ listado/index.js
@@ -5,6 +5,17 @@ const app=require('electron').app;
 const path=require('path');
 const url=require('url');
 const $ = require('jquery');
+
+function datos(nombre,genero,foto,direccion,telefono)
+{
+	this.nombre    = nombre;
+	this.genero    = genero;
+	this.foto      = foto;
+	this.direccion = direccion;
+	this.telefono  = telefono;
+}
+
+var usuarios = new Array(20);
 function inicia()
 {
 	$.ajax({
@@ -12,18 +23,38 @@ function inicia()
 	  dataType: 'json',
 	  success: function(data) {
 	  	var resultado = "";
-	  	var nombre = "";
+	  	var nombre    = "";
+	  	var genero    = "";
+	  	var direccion = "";
+	  	var telefono  = "";
+	  	var foto      = "";
 	  	for(var i=0;i<20;i++)
 	  	{
 	  		nombre = data.results[i].name.first+" "+data.results[i].name.last;
+	  		genero = data.results[i].gender;
+	  		direccion = data.results[i].location.street;
+	  		telefono = data.results[i].phone;	  		
 	  		foto = data.results[i].picture.medium;
-	  		resultado="<li><img src="+foto+">"+nombre+"</li>";
+	  		resultado="<li><img src="+foto+">"+nombre+"<button id='"+i+"'>Detalle</button></li>";
 	  		$("#lstUsuarios").append(resultado);
+	  		usuarios[i] = new datos(nombre,genero,foto,direccion,telefono);
 	  	}	  	
 	  }
 	});
 }
+
+function botonDetalle(){
+	alert(usuarios[this.id].nombre);
+	alert(usuarios[this.id].genero);
+	alert(usuarios[this.id].foto);
+}
+
+$("body").on("click","li > button",botonDetalle);
 inicia();
+
+
+
+
 
 
 
