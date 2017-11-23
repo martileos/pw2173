dc6286ff75d74a46b908533e99a4f9f34200bd30
martileos <martinevarez@gmail.com>
2017-11-23 16:21:04 -0700

    Llamada a funcion para imprimir en PDF

--- listado/main.js
+++ listado/main.js
@@ -4,6 +4,17 @@
 const {app,BrowserWindow} = require('electron');
 const path = require('path'); //Muestra la ruta del archivo
 const url = require('url'); //Carga una página
+//Constantes para PDF
+const electron = require('electron');
+//Sistema de archivos
+const fs = require('fs');
+//Acceso al sistema operativo
+const os = require('os');
+//Para declarar una función remota
+const ipc = electron.ipcMain;
+//Acceso a la terminal-línea de comandos
+const shell = electron.shell;
+
 // ECMASCRIPT = 6
 let PantallaPrincipal;
 //Objeto global para compartir datos
@@ -27,7 +38,33 @@ function muestraPantallaPrincipal(){
 	PantallaPrincipal.show();
 }
 
+//Evento para PDF (declaración) 
+ipc.on('print-to-pdf',function(event){
+	const pdfPath=path.join(os.tmpdir(),'print.pdf')
+	const win=BrowserWindow.fromWebContents(event.sender)
+	win.webContents.printToPDF({},function(error,data){
+		if(error) throw error
+		fs.writeFile(pdfPath,data,function(error){
+			if(error){
+				throw error
+			}
+			shell.openExternal('file://'+pdfPath)
+			win.close()
+		})
+	})
+});
+
 app.on('ready',muestraPantallaPrincipal)
+
+
+
+
+
+
+
+
+
+
 
 
 
